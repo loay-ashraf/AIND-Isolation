1794c8c2afc86d78f6951ef03153d390433ec653
Loay Ashraf <loay.ashraf.96@gmail.com>
2019-03-27 20:45:55 +0200

    First commit: upload files

---
+++ agent_test.py
@@ -0,0 +1,29 @@
+"""This file is provided as a starting template for writing your own unit
+tests to run and debug your minimax and alphabeta agents locally.  The test
+cases used by the project assistant are not public.
+"""
+
+import unittest
+
+import isolation
+import game_agent
+
+from importlib import reload
+
+
+class IsolationTest(unittest.TestCase):
+    """Unit tests for isolation agents"""
+
+    def setUp(self):
+        reload(game_agent)
+        self.player1 = game_agent.MinimaxPlayer()
+        self.player2 = game_agent.MinimaxPlayer()
+        self.game = isolation.Board(self.player1, self.player2)
+    
+    def play(self):
+        print("starting game")
+        self.game.play()
+        self.assertEqual(True, True)
+
+if __name__ == '__main__':
+    unittest.main()
